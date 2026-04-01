565379acd47b401a7161d19728a76c3054e98ff5
Julia-Mannex <julia.7595107@aluno.mg.gov.br>
2026-04-01 16:10:24 +0000

    alterações

--- js/script.js
+++ js/script.js
@@ -18,3 +18,28 @@ function curtir() {
 }
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
+
+
+let dislikeCount = 0;
+let descurtir = false;
+
+function descurtir() {
+  
+  if(descurtido == false){
+    likeCount++;
+    descurtido = true;
+    document.getElementById("dislikeCount").innerText = dislikeCount;  
+  }else{
+    likeCount--;
+    descurtido = false;
+    document.getElementById("dislikeCount").innerText = dislikeCount; 
+  }
+    
+
+
+}
+
+document.getElementById("dislikeBtn").addEventListener("click", descurtir);
+
+
+
